c4057153f48a26b567a2cb4c8186587e66657a15
Mahdi <mahdihosseinzadeh.uut1401@gmail.com>
2024-07-23 09:14:01 +0330

    Adding Product.java

---
+++ src/main/java/Products_Orders/Product.java
@@ -0,0 +1,118 @@
+package Products_Orders;
+
+import java.util.Objects;
+
+public class Product {
+    private int productId;
+    private String productName;
+    private String productDescription;
+    private int quantity;
+    private double price;
+    private String supplierId;
+
+    public Product(int productId, String productName, String productDescription, int quantity, double price, String supplierId) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.quantity = quantity;
+        this.price = price;
+        this.supplierId = supplierId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(String supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public void addStock(int quantity) {
+        if (quantity > 0) {
+            this.quantity += quantity;
+        } else{
+            throw new IllegalArgumentException("Quantity must be greater than 0");
+        }
+    }
+
+    public void removeStock(int quantity) {
+        if (quantity > 0) {
+            this.quantity -= quantity;
+        } else{
+            throw new IllegalArgumentException("Quantity must be greater than 0");
+        }
+    }
+
+    public void updatePrice(double price) {
+        if (price > 0) {
+            this.price += price;
+        } else{
+            throw new IllegalArgumentException("Price must be greater than 0");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(productId , product.productId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId= " + getProductId() +
+                ", productName=' " + getProductName() + '\'' +
+                ", productDescription=' " + getProductDescription() + '\'' +
+                ", quantity= " + getQuantity() +
+                ", price= " + getPrice() +
+                ", supplierId=' " + getSupplierId() + '\'' +
+                '}';
+    }
+}
